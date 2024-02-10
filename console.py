121647d098568c1f7b1976f957ee61d99a6fe218
MalekAjil <aboanasajil@gmail.com>
2024-02-10 12:18:09 -0800

    task6

--- console.py
+++ console.py
@@ -14,7 +14,10 @@ class HBNBCommand(cmd.Cmd):
     def do_help(self, args):
         """Returns information about Class methods"""
         super().do_help(args)
-        return False
+
+    def emptyline(self):
+        """Do nothing"""
+        return False 
 
     def do_quit(self, args):
         """Quit command to exit the program"""
