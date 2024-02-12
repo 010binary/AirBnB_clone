3a6866739221dab3362df7b786db256da82d86a5
MalekAjil <aboanasajil@gmail.com>
2024-02-11 17:51:30 -0800

    task6

--- console.py
+++ console.py
@@ -27,5 +27,71 @@ class HBNBCommand(cmd.Cmd):
         """EOF command to exit the program"""
         return True
 
+    def do_create(self, args):
+        """Creates a new instance of BaseModel,
+	saves it (to the JSON file) and prints the id\n
+	"""
+        if not args:
+            print("** class name missing **")
+        elif exist(args[0]):
+            print("** class doesn't exist **")
+        else:
+            bm = BaseModel()
+            bm.save()
+
+    def do_show(self, args):
+        """Prints the string representation of an instance based
+	on the class name and id\n
+	"""
+        if not args:
+            print("** class name missing **")
+        elif exist(args[0]):
+            print("** class doesn't exist **")
+        elif not args[1]:
+            print("** instance id missing **")
+        else:
+            bm = BaseModel()
+
+    def do_destroy(self, args):
+        """Deletes an instance based on the class name and id
+	(save the change into the JSON file).\n
+	"""
+        if not args:
+            print("** class name missing **")
+        elif exist(args[0]):
+            print("** class doesn't exist **")
+        elif not args[1]:
+            print("** instance id missing **")
+        else:
+            bm = BaseModel()
+
+    def do_all(self, args):
+        """Prints all string representation of all instances based
+	or not on the class name.\n
+	"""
+        if exist(args[0]):
+            print("** class doesn't exist **")
+        else:
+            all_objs = storage.all()
+            print("-- Reloaded objects --")
+            for obj_id in all_objs.keys():
+                obj = all_objs[obj_id]
+                print(obj)
+
+    def do_update(self, args):
+        '''Updates an instance based on the class name and id
+	by adding or updating attribute
+	(save the change into the JSON file)
+	Usage: update <class name> <id> <attribute name> "<attribute value>"
+	'''
+        if not args:
+            print("** class name missing **")
+        elif exist(args[0]):
+            print("** class doesn't exist **")
+        elif not args[1]:
+            print("** instance id missing **")
+        else:
+            bm = BaseModel()
+
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
